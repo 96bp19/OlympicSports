15dca5576572ee058fb51510217fdc5a0761ce9c
96bp19 <lanthoo170@gmail.com>
2020-01-20 15:09:47 +0545

    Javaline Throw Added

---
+++ OlympicSports/Assets/Scripts/Javaline.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Javaline : MonoBehaviour
+{
+
+    Transform target;
+    bool calculateDistanceStart;
+    public float currentDistance;
+    void Update()
+    {
+        if (transform.position.y <1f)
+        {
+            calculateDistanceStart = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+       
+        if (calculateDistanceStart)
+        {
+            currentDistance = Mathf.Abs(target.transform.position.z - transform.position.z);
+        }
+
+        if (!GetComponent<Rigidbody>().isKinematic)
+        {
+            Quaternion rot = Quaternion.LookRotation(GetComponent<Rigidbody>().velocity.normalized);
+            transform.rotation = rot;
+
+        }
+    }
+
+    public void setTarget(Transform target)
+    {
+        this.target = target;
+        calculateDistanceStart = true;
+    }
+}
---
+++ OlympicSports/Assets/Scripts/Tap Zones/JavalineThrowZone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavalineThrowZone : ATapZone
+{
+    [SerializeField] private Transform javalineObjectPrefab;
+    public override void DoInputAction(float accuracy)
+    {
+        player.ResetPlayerSpeed();
+        ThrowJavaline(accuracy);
+    }
+    
+    void ThrowJavaline(float accuracy)
+    {
+        float angleToThrowAt = calculateRandomAngleBasedOnAccuracy(accuracy);
+        float throwPower = Random.Range(55, 60);
+        Vector3 launchVel = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angleToThrowAt), Mathf.Cos(Mathf.Deg2Rad * angleToThrowAt)) * throwPower;
+        Debug.Log("launch vel : " + launchVel);
+        Transform javaline = Instantiate(javalineObjectPrefab, player.transform.position, Quaternion.identity);
+        Rigidbody javaline_rb = javaline.GetComponent<Rigidbody>();
+        javaline_rb.velocity = launchVel;
+        javaline.GetComponent<Javaline>().setTarget(player.transform);
+        Debug.Log("thrown with the angle of : " + angleToThrowAt);
+
+    }
+
+    float calculateRandomAngleBasedOnAccuracy(float accuracy)
+    {
+        float randomAngle = 0;
+        if (accuracy <0.5f)
+        {
+            //fair
+            randomAngle = RandomBool() ? Random.Range(25, 35) : Random.Range(60, 75); 
+            
+        }
+        else if (accuracy <0.8f)
+        {
+            // good
+            randomAngle = RandomBool() ? Random.Range(35, 42) : Random.Range(48, 60); 
+            
+        }
+        else
+        {
+            randomAngle =  Random.Range(42, 48); 
+        }
+        return randomAngle;
+    }
+
+
+    bool RandomBool()
+    {
+        return Random.Range(0, 2) == 0;
+    }
+}
