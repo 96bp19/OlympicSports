ef009086d8027c6671b9e75ce57a989fca080181
96bp19 <lanthoo170@gmail.com>
2020-03-11 16:03:46 +0545

    Trying Cloth Customization

---
+++ Assets/Scripts/Usable/SimpleClotheCustomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleClotheCustomizer : MonoBehaviour
+{
+    public ClothInformation clothInfo;
+    public SkinnedMeshRenderer upperBodySkinRenderer, lowerBodySkinRenderer, headSkinRenderer, FeetSkinRenderer;
+
+    public void ChangeClothData()
+    {
+        
+    }
+
+}
